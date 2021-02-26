51b7be3d26e9701f9a52be0cdabfc7af13f32df8
Alexander Putilin <jows.17@mail.ru>
2021-02-26 23:48:01 +0300

    Created info page about entity, more partials and cataogRepository

--- nVideo/Controllers/CatalogController.cs
+++ nVideo/Controllers/CatalogController.cs
@@ -12,31 +12,39 @@ namespace nVideo.Controllers
     public class CatalogController : Controller
     {
 
-        private readonly IAllCatalog _allCatalog;
+        private readonly IAllCatalog _catalog;
         private readonly ILogger<CatalogController> _logger;
 
-        public CatalogController(IAllCatalog catalog, ILogger<CatalogController> logger)
-        {
-            _allCatalog = catalog;
+        public CatalogController(IAllCatalog catalog, ILogger<CatalogController> logger){
+            _catalog = catalog;
             _logger = logger;
         }
 
-        public ViewResult List()
-        {
+        public ViewResult List(){
             var model = new CatalogViewModel();
-            model.Entities = _allCatalog.AllEntnty;
+            model.Entities = _catalog.GetAllEntnty;
 
             return View(model);
         }
 
-        [Route("Catalog/View/{id}")]
-        public ViewResult View(int? id)
-        {
-            if (id.HasValue)
-            {
-                // success
+        [Route("Catalog/CategoryFilter/{category}")]
+        public IActionResult CategoryFilter(string category){
+            if (!string.IsNullOrEmpty(category)){
+                var model = new CatalogViewModel();
+
+                model.Entities = _catalog.GetCategoryMembers(category);
+
+                return View("List", model);
             }
-            return View();
+            throw new ArgumentNullException("Missing parameter: string category");
+        }
+
+        [Route("Catalog/About/{id}")]
+        public ViewResult About(int? id){
+            if (id.HasValue){
+                return View(_catalog.GetItemById(id));
+            }
+            throw new ArgumentNullException("Missing parameter: int id");
         }
     }
 }
--- nVideo/DATA/Repository/CatalogRepository.cs
+++ nVideo/DATA/Repository/CatalogRepository.cs
@@ -10,17 +10,72 @@ namespace nVideo.DATA.Repository
 {
     public class CatalogRepository : IAllCatalog
     {
-
         private readonly AppDBContext _context;
+
+        private static IQueryable<Catalog_Entity> _entities;
+        public IQueryable<Catalog_Entity> Entities {
+            get {
+                if (_entities == _entities) {
+                    _entities = _context.Entities
+                        .Include(e => e.Images);
+                }
+                return _entities;
+            }
+        }
 
         public CatalogRepository(AppDBContext context) {
             _context = context;
         }
 
-        public IEnumerable<Catalog_Entity> AllEntnty =>
-            _context.Entities
-            .Include(e => e.Images)
-            .Include(e => e.Attributes)
-            .ThenInclude(e => e.Value);
+        public IEnumerable<Catalog_Entity> GetCarouselItems {
+            get {
+                return GetFeaturedItems;
+            }
+        }
+
+        public IEnumerable<Catalog_Entity> GetFeaturedItems{
+            get{
+                return Entities.OrderByDescending(e => e.Raiting)
+                    .Take(3);
+            }
+        }
+
+        public IEnumerable<Catalog_Entity> GetNewItems{
+            get{
+                return Entities.OrderByDescending(e => e.InStock)
+                    .Take(8);
+            }
+        }
+
+        public Catalog_Entity GetRandomItem{
+            get{
+                var rnd = new Random(DateTime.Now.Millisecond);
+                return Entities.ToArray()[rnd.Next(0, Entities.Count())];
+            }
+        }
+
+        public IEnumerable<Catalog_Entity> GetCategoryMembers(string category) {
+            return Entities.Include(e => e.Category)
+                .Where(e => e.Category.CategoryName.Equals(category));
+        }
+
+        public Catalog_Entity GetItemById(int? id){
+            if (id.HasValue){
+                IQueryable<Catalog_Entity> target;
+
+                return (target = Entities.Where(e => e.Id.Equals(id)))
+                                         .Count() == 0
+                                              ? throw new ArgumentException("Id is not exit")
+                                              : target.Include(t => t.Attributes)
+                                                      .ThenInclude(t => t.Value)
+                                                      .First();                                            
+            }
+            throw new ArgumentNullException("Missing parameter: int id");
+        }
+
+
+        public IEnumerable<Catalog_Entity> GetAllEntnty =>
+             Entities.Include(e => e.Attributes)
+                     .ThenInclude(e => e.Value);
     }
 }
