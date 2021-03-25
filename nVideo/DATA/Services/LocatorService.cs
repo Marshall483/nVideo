d2bc6ec4f9666796984cdee75681aaa1bea3f87e
Danil <danikavsilev@gmail.com>
2021-03-26 02:14:44 +0300

    забыл пакет пагрузить

--- nVideo/DATA/Services/LocatorService.cs
+++ nVideo/DATA/Services/LocatorService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using IpInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace nVideo.DATA.Services
