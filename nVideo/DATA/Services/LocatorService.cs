9807f458101bd728662a089b981725ace279efdb
Danil <danikavsilev@gmail.com>
2021-03-26 02:09:27 +0300

    автоопределение города

---
+++ nVideo/DATA/Services/LocatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nVideo.DATA.Services
+{
+    public class LocatorService
+    {
+        private readonly string token = "1432677e806667";
+        public async Task<string> GetyCityAsync()
+        {
+            var client = new HttpClient();
+            var api = new IpInfoApi(token, client);
+            var response = await api.GetCurrentInformationAsync();
+
+            return response.City;
+        }
+    }
+}
