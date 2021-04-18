0c42fdec05ac54dceb9ebae93a57e0cd90419532
Niyaz <nieandevar@mail.ru>
2021-04-18 22:25:14 +0300

    add  creating user

--- nVideo/Controllers/AdminPanelController.cs
+++ nVideo/Controllers/AdminPanelController.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using MailKit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using nVideo.DATA.ControllerModels;
+using nVideo.DATA.Services;
 using nVideo.Models;
 
 namespace nVideo.Controllers
@@ -13,10 +18,14 @@ namespace nVideo.Controllers
     public class AdminPanelController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly EmailSenderService _sender;
+        private readonly ILogger<AccountController> _logger;
 
-        public AdminPanelController(UserManager<User> userManager)
+        public AdminPanelController(UserManager<User> userManager, EmailSenderService sender, ILogger<AccountController> logger)
         {
             _userManager = userManager;
+            _sender = sender;
+            _logger = logger;
         }
         // GET
         public async Task<IActionResult> Index()
@@ -32,24 +41,85 @@ namespace nVideo.Controllers
             
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> Ban(string useremail)
         {
             if(useremail== null) return View("AdminFail");
             var user = await _userManager.FindByEmailAsync(useremail);
             if (user != null) return View("AdminFail");
             await _userManager.AddToRoleAsync(user, "baned");
-            return View("Sucksess");
+            return View("Success");
         }
+        [HttpPost]
         public async Task<IActionResult> UnBan(string useremail)
         {
             if(useremail== null) return View("AdminFail");
             var user = await _userManager.FindByEmailAsync(useremail);
             if (user != null) return View("AdminFail");
             await _userManager.AddToRoleAsync(user, "user");
-            return View("Sucksess");
+            return View("Success");
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddAdmin(string useremail)
+        {
+            if(useremail== null) return View("AdminFail");
+            var user = await _userManager.FindByEmailAsync(useremail);
+            if (user != null) return View("AdminFail");
+            await _userManager.AddToRoleAsync(user, "admin");
+            return View("Success");
         }
         
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel registerModel){
+            if (ModelState.IsValid){
+                var user = await _userManager.FindByNameAsync(registerModel.Email);
+               
+                if (user != null) {
+                    ModelState.AddModelError("UniqMailError", "User with such login already exist");
+                    return View();
+                }
+
+                var newUser = new User{
+                    Email = registerModel.Email,
+                    UserName = registerModel.Email,
+                    Profile = new UserProfile()
+                };
+
+
+                 var res = await _userManager.CreateAsync(newUser, registerModel.Password);
+
+                if (res.Succeeded)
+                {
+                    // token generation
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+                    var callbackUrl = Url.Action(
+                        "ConfirmEmail",
+                        "Account",
+                        new { userId = newUser.Id, code = code },
+                        protocol: HttpContext.Request.Scheme);
+
+                    try{
+                        await _sender.SendEmailAsync(registerModel.Email, "Confirm your account",
+                            $"Verify your email on click by <a href='{callbackUrl}'>link</a>");
+                    }
+                    catch (CommandException cEx){
+                        _logger.LogError($"Err with User - {newUser.Email}. \n {cEx.Message}");
+                        ModelState.AddModelError("SenderError", "Specified Email is not exist");
+                        return View(registerModel);
+                    }
+                    catch (Exception suddenEx){
+                        _logger.LogError($"Err with User - {newUser.Email}. \n {suddenEx.Message}");
+                        ModelState.AddModelError("SenderError", "An error occured while sending email.");
+                        return View(registerModel);
+                    }
+
+                    ViewBag.Message = "To complete the registration, check your email address and follow the link provided in the email";
+                    return View("OnEmailConfirm");
+                }
+            }
+            return View();
+        }
         
         
     }
