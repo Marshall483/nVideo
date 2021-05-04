9a53c0a12188ae2f250e8c83bb253b5090a85ac5
Danil <danikavsilev@gmail.com>
2021-05-04 06:09:21 +0300

    Фильтры поиска и нормальные города

---
+++ nVideo/Components/CitiesViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nVideo.DATA;
+using nVideo.DATA.Services;
+using nVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nVideo.Components
+{
+    public class CitiesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public CitiesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var city = HttpContext.Request.Cookies["City"];
+            if (string.IsNullOrEmpty(city))
+            {
+                city = await LocatorService.GetyCityAsync();
+                if (_context.Cities.FirstOrDefault(x => x.Name.Equals(city)) == null)
+                    city = "Не удалось определить город";
+                HttpContext.Response.Cookies.Append("City", city);
+            }
+            var cities = _context.Cities;
+            var model = new CitiesViewModel(city, cities);
+            return View(model);
+        }
+    }
+}
