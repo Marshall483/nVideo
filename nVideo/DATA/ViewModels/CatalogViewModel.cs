a1919757e5ad2dbacb010f1fd7bcb501a163cbef
Danil <danikavsilev@gmail.com>
2021-05-24 17:41:20 +0300

    двавай блядь

--- nVideo/DATA/ViewModels/CatalogViewModel.cs
+++ nVideo/DATA/ViewModels/CatalogViewModel.cs
@@ -6,5 +6,7 @@ namespace nVideo.DATA.ViewModels
     public class CatalogViewModel
     {
         public IEnumerable<Catalog_Entity> Entities { get; set; }
+        public Dictionary<string, string> Dict { get; set; }
+
     }
 }
