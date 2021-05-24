fae278b562f939250988cfba17b7a3e315370d38
Danil <danikavsilev@gmail.com>
2021-05-24 17:03:44 +0300

    абоба

--- nVideo/Startup.cs
+++ nVideo/Startup.cs
@@ -81,7 +81,7 @@ namespace nVideo
             app.Use(async (context, next) =>
             {
                 if (!context.Request.Cookies.ContainsKey("City"))
-                    context.Response.Cookies.Append("City", await LocatorService.GetyCityAsync());
+                    context.Response.Cookies.Append("City", Locator.Response);
                 await next.Invoke();
             });
             app.UseEndpoints(endpoints =>
